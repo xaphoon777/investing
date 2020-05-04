8f0783115e2e1f7033c2cd32244e68c8b643fa10
Volobuev Alex <A.Volobuev@x5.ru>
2020-05-04 15:46:34 +0300

    Актуализировал тест

--- src/test/java/investing/ThreeLevelGraphTest.java
+++ src/test/java/investing/ThreeLevelGraphTest.java
@@ -8,8 +8,8 @@ public class ThreeLevelGraphTest {
 
   @Test
   public void build() {
-      float[] input = {100,105,107};
-      float[] expected = {100,105,107};
+      float[] input = {100,105,107,104,102,108,106,104,109,104,100,103,102,99,105,97};
+      float[] expected = {100,105,107,108,109,104,100,99,97};
       final float[] actual = ThreeLevelGraph.build(input);
       assertArrayEquals(expected, actual, 0.1f);
   }
