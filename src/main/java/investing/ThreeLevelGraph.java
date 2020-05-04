5f17f7e3e581b023a3aa4820f5fbe0e4bdc7d3e1
AKS <my@no.com>
2020-05-04 20:02:18 +0300

    updateExtemums resolved

--- src/main/java/investing/ThreeLevelGraph.java
+++ src/main/java/investing/ThreeLevelGraph.java
@@ -12,23 +12,30 @@ public class ThreeLevelGraph {
 
     public static List<Float> build(List<Float> input) {
         List<Float> output = new ArrayList<>();
-        int days = 3;
+        int days = 0;
 
         for (float current : input) {
-            if (output.size() == 0) {
+
+            if ((output.size() == 0) && (days < 3)) {
                 output.add(current);
+                days += 1;
                 min = current;
                 max = current;
+
             } else {
                 if (current < min) {
                     min = current;
-                    updateExtemums(output.subList(output.size()-days-1,output.size()-1));
+                    days += 1;
+                    updateExtemums(output.subList(output.size() - (days - 1), output.size() - 1));
                     output.add(current);
+
                 } else {
                     if (current > max) {
                         max = current;
-                        updateExtemums(output.subList(output.size()-days-1,output.size()-1));
+                        days += 1;
+                        updateExtemums(output.subList(output.size() - (days - 1), output.size() - 1));
                         output.add(current);
+
                     }
 
                 }
@@ -39,8 +46,16 @@ public class ThreeLevelGraph {
         return output;
     }
 
-    private static void updateExtemums ( List <Float> tail) {
-        max = tail.stream().max(Float::compareTo).get();
-        min = tail.stream().min(Float::compareTo).get();
+    private static void updateExtemums(List<Float> tail) {
+        for (Float aFloat : tail) {
+            if (aFloat < min) {
+                min = aFloat;
+            }
+            if (aFloat > max) {
+                max = aFloat;
+            }
+        }
+//        max = tail.stream().max(Float::compareTo).get();
+//        min = tail.stream().min(Float::compareTo).get();
     }
 }
